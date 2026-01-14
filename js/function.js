6973aff4ea58eecde1d97c5c3ce9c98351f8cc5c
Basil Pulikuth <basilpulikuth1@Basils-MacBook-Air.local>
2026-01-14 18:12:56 +0530

    Project Structure Initialized

---
+++ js/function.js
@@ -0,0 +1,390 @@
+(function ($) {
+    "use strict";
+	
+	var $window = $(window); 
+	var $body = $('body'); 
+
+	/* Preloader Effect */
+	$window.on('load', function(){
+		$(".preloader").fadeOut(600);
+	});
+
+	/* Sticky Header */	
+	if($('.active-sticky-header').length){
+		$window.on('resize', function(){
+			setHeaderHeight();
+		});
+
+		function setHeaderHeight(){
+	 		$("header.main-header").css("height", $('header .header-sticky').outerHeight());
+		}	
+	
+		$window.on("scroll", function() {
+			var fromTop = $(window).scrollTop();
+			setHeaderHeight();
+			var headerHeight = $('header .header-sticky').outerHeight()
+			$("header .header-sticky").toggleClass("hide", (fromTop > headerHeight + 100));
+			$("header .header-sticky").toggleClass("active", (fromTop > 600));
+		});
+	}	
+	
+	/* Slick Menu JS */
+	$('#menu').slicknav({
+		label : '',
+		prependTo : '.responsive-menu'
+	});
+
+	if($("a[href='#top']").length){
+		$(document).on("click", "a[href='#top']", function() {
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+			return false;
+		});
+	}
+
+	/* Hero Slider Layout JS */
+	const hero_slider_layout = new Swiper('.hero-slider-layout .swiper', {
+		effect: 'fade',
+		slidesPerView : 1,
+		speed: 1000,
+		spaceBetween: 0,
+		loop: true,
+		autoplay: {
+			delay: 4000,
+		},
+		pagination: {
+			el: '.hero-pagination',
+			clickable: true,
+		},
+	});
+
+	/* testimonial Slider JS */
+	if ($('.testimonial-slider').length) {
+		const testimonial_slider = new Swiper('.testimonial-slider .swiper', {
+			slidesPerView : 1,
+			speed: 1000,
+			spaceBetween: 30,
+			centeredSlides: true,
+			loop: true,
+			autoplay: {
+				delay: 5000,
+			},
+			pagination: {
+				el: '.testimonial-pagination',
+				clickable: true,
+			},
+			navigation: {
+				nextEl: '.testimonial-button-next',
+				prevEl: '.testimonial-button-prev',
+			},
+			breakpoints: {
+				768:{
+					slidesPerView: 2,
+				},
+				991:{
+					slidesPerView: 3,
+				}
+			}
+		});
+	}
+
+	/* Youtube Background Video JS */
+	if ($('#herovideo').length) {
+		var myPlayer = $("#herovideo").YTPlayer();
+	}
+
+	/* Init Counter */
+	if ($('.counter').length) {
+		$('.counter').counterUp({ delay: 6, time: 3000 });
+	}
+
+	/* Image Reveal Animation */
+	if ($('.reveal').length) {
+        gsap.registerPlugin(ScrollTrigger);
+        let revealContainers = document.querySelectorAll(".reveal");
+        revealContainers.forEach((container) => {
+            let image = container.querySelector("img");
+            let tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: container,
+                    toggleActions: "play none none none"
+                }
+            });
+            tl.set(container, {
+                autoAlpha: 1
+            });
+            tl.from(container, 1, {
+                xPercent: -100,
+                ease: Power2.out
+            });
+            tl.from(image, 1, {
+                xPercent: 100,
+                scale: 1,
+                delay: -1,
+                ease: Power2.out
+            });
+        });
+    }
+
+	/* Text Effect Animation */
+	if ($('.text-anime-style-1').length) {
+		let staggerAmount 	= 0.05,
+			translateXValue = 0,
+			delayValue 		= 0.5,
+		   animatedTextElements = document.querySelectorAll('.text-anime-style-1');
+		
+		animatedTextElements.forEach((element) => {
+			let animationSplitText = new SplitText(element, { type: "chars, words" });
+				gsap.from(animationSplitText.words, {
+				duration: 1,
+				delay: delayValue,
+				x: 20,
+				autoAlpha: 0,
+				stagger: staggerAmount,
+				scrollTrigger: { trigger: element, start: "top 85%" },
+				});
+		});		
+	}
+	
+	if ($('.text-anime-style-2').length) {				
+		let	 staggerAmount 		= 0.03,
+			 translateXValue	= 20,
+			 delayValue 		= 0.1,
+			 easeType 			= "power2.out",
+			 animatedTextElements = document.querySelectorAll('.text-anime-style-2');
+		
+		animatedTextElements.forEach((element) => {
+			let animationSplitText = new SplitText(element, { type: "chars, words" });
+				gsap.from(animationSplitText.chars, {
+					duration: 1,
+					delay: delayValue,
+					x: translateXValue,
+					autoAlpha: 0,
+					stagger: staggerAmount,
+					ease: easeType,
+					scrollTrigger: { trigger: element, start: "top 85%"},
+				});
+		});		
+	}
+	
+	if ($('.text-anime-style-3').length) {		
+		let	animatedTextElements = document.querySelectorAll('.text-anime-style-3');
+		
+		 animatedTextElements.forEach((element) => {
+			//Reset if needed
+			if (element.animation) {
+				element.animation.progress(1).kill();
+				element.split.revert();
+			}
+
+			element.split = new SplitText(element, {
+				type: "lines,words,chars",
+				linesClass: "split-line",
+			});
+			gsap.set(element, { perspective: 400 });
+
+			gsap.set(element.split.chars, {
+				opacity: 0,
+				x: "50",
+			});
+
+			element.animation = gsap.to(element.split.chars, {
+				scrollTrigger: { trigger: element,	start: "top 90%" },
+				x: "0",
+				y: "0",
+				rotateX: "0",
+				opacity: 1,
+				duration: 1,
+				ease: Back.easeOut,
+				stagger: 0.02,
+			});
+		});		
+	}
+
+	/* Parallaxie js */
+	var $parallaxie = $('.parallaxie');
+	if($parallaxie.length && ($window.width() > 991))
+	{
+		if ($window.width() > 768) {
+			$parallaxie.parallaxie({
+				speed: 0.55,
+				offset: 0,
+			});
+		}
+	}
+
+	/* Contact form validation */
+	var $contactform = $("#contactForm");
+	$contactform.validator({focus: false}).on("submit", function (event) {
+		if (!event.isDefaultPrevented()) {
+			event.preventDefault();
+			submitForm();
+		}
+	});
+
+	function submitForm(){
+		/* Ajax call to submit form */
+		$.ajax({
+			type: "POST",
+			url: "form-process.php",
+			data: $contactform.serialize(),
+			success : function(text){
+				if (text === "success"){
+					formSuccess();
+				} else {
+					submitMSG(false,text);
+				}
+			}
+		});
+	}
+
+	function formSuccess(){
+		$contactform[0].reset();
+		submitMSG(true, "Message Sent Successfully!")
+	}
+
+	function submitMSG(valid, msg){
+		if(valid){
+			var msgClasses = "h4 text-success";
+		} else {
+			var msgClasses = "h4 text-danger";
+		}
+		$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+	}
+	/* Contact form validation end */
+
+	/* Animated Wow Js */	
+	new WOW().init();
+
+	/* Popup Video */
+	if ($('.popup-video').length) {
+		$('.popup-video').magnificPopup({
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+			fixedContentPos: true
+		});
+	}
+
+	/* About Us Body Start */
+	var $about_us_body = $('.about-us-body');
+	if ($about_us_body.length) {
+		var $about_body_item = $about_us_body.find('.about-body-item');
+
+		if ($about_body_item.length) {
+			$about_body_item.on({
+				mouseenter: function () {
+					if (!$(this).hasClass('active')) {
+						$about_body_item.removeClass('active'); 
+						$(this).addClass('active'); 
+					}
+				},
+				mouseleave: function () {
+					// Optional: Add logic for mouse leave if needed
+				}
+			});
+		}
+	}
+	/* About Us Body End */
+
+	/* Product Item List Start */
+	var $product_item_list = $('.product-item-list');
+	if ($product_item_list.length) {
+		var $product_item = $product_item_list.find('.product-item');
+
+		if ($product_item.length) {
+			$product_item.on({
+				mouseenter: function () {
+					if (!$(this).hasClass('active')) {
+						$product_item.removeClass('active'); 
+						$(this).addClass('active'); 
+					}
+				},
+				mouseleave: function () {
+					// Optional: Add logic for mouse leave if needed
+				}
+			});
+		}
+	}
+	/* Product Item List End */
+
+	/* Features Item List Start */
+	var $features_item_list = $('.features-item-list');
+	if ($features_item_list.length) {
+		var $feature_item = $features_item_list.find('.features-item');
+
+		if ($feature_item.length) {
+			$feature_item.on({
+				mouseenter: function () {
+					if (!$(this).hasClass('active')) {
+						$feature_item.removeClass('active'); 
+						$(this).addClass('active'); 
+					}
+				},
+				mouseleave: function () {
+					// Optional: Add logic for mouse leave if needed
+				}
+			});
+		}
+	}
+	/* Features Item List End */
+
+	/* Features Item List Start */
+	var $testimonial_item_list = $('.testimonial-item-list');
+	if ($testimonial_item_list.length) {
+		var $testimonial_item = $testimonial_item_list.find('.testimonial-item');
+
+		if ($testimonial_item.length) {
+			$testimonial_item.on({
+				mouseenter: function () {
+					if (!$(this).hasClass('active')) {
+						$testimonial_item.removeClass('active'); 
+						$(this).addClass('active'); 
+					}
+				},
+				mouseleave: function () {
+					// Optional: Add logic for mouse leave if needed
+				}
+			});
+		}
+	}
+	/* Features Item List End */
+
+	/* counting down Timer Start */
+	if(document.getElementById("days")) {
+		// Set the date we're counting down to (2 days from now)
+		const countDownDate = new Date().getTime() + (5 * 24 * 60 * 60 * 1000);
+
+		// Update the countdown every 1 second
+		const x = setInterval(function() {
+		// Get today's date and time
+		const now = new Date().getTime();
+
+		// Find the distance between now and the countdown date
+		const distance = countDownDate - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		// Display the result
+		document.getElementById("days").innerHTML = days.toString().padStart(2, '0');
+		document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+		document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+		document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+
+		// If the countdown is finished, write some text
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("days").innerHTML = "00";
+			document.getElementById("hours").innerHTML = "00";
+			document.getElementById("minutes").innerHTML = "00";
+			document.getElementById("seconds").innerHTML = "00";
+		}
+		}, 1000);
+	}
+	/* counting down Timer End */
+
+})(jQuery);
